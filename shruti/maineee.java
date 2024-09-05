f968db5ce80f6f23ee7561a3ae8150b19628915a
shruti <122924152+shruti-ugale-code@users.noreply.github.com>
2024-09-05 11:50:14 +0530

    Add files via upload

---
+++ shruti/maineee.java
@@ -0,0 +1,20 @@
+package com.shruti;
+
+public class maineee {
+    public static void main(String[] args) {
+        nodee list=new nodee();
+        list.Insert(5);
+        list.Insert(5);
+        list.Insert(5);
+        list.Insert(5);
+        list.Insert(5);
+        list.Insert(5);
+        list.Insert(5);
+        list.Insert(5);
+
+        list.display();
+
+
+
+    }
+}
---
+++ shruti/nodee.java
@@ -0,0 +1,44 @@
+package com.shruti;
+
+public class nodee {
+    private node head;
+    private node tail;
+    private int size;
+     public nodee(){
+         this.size=0;
+
+     }
+     public void Insert(int val){
+         node n=new node(val);
+         n.next=head;
+         head=n;
+
+         if(tail==null){
+             tail=head;
+         }
+         size+=1;
+
+     }
+     public void display(){
+         node temp=head;
+         while (temp!=null){
+             System.out.print(temp.val+" --> ");
+             temp=temp.next;
+         }
+         System.out.println("end");
+     }
+
+
+    private class node{
+        private int val;
+        private node next;
+        public node(int val){
+            this.val=val;
+        }
+        public node(int val,node next){
+            this.val=val;
+            this.next=next;
+        }
+    }
+
+}
